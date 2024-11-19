426564dc6686f4282ccafbcb3c50325257aaca76
Madhuranga Bandara <madurangabandara2346@gmail.com>
2024-11-19 21:22:31 +0530

    create student information portal

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import {students} from './data/studentsbd';
+import StudentTable from './components/StudentTable';
+function App() {
+  return (
+    <div className="App">
+      <h1>Student Information Portal</h1>
+      <hr/>
+      <table width="100%">
+        <tbody>
+          <tr>
+            <td>
+              <StudentTable students={students}/>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StudentTable.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import '../assets/css/compo.css'
+import Profile from './Profile';
+export default function StudentTable(props){
+    const [stu, setStu] = useState(props.students[0]);
+    return(
+        
+          <div className="outerDiv">
+            <div className="leftDiv">
+                <table border={1}>
+                    <thead>
+                        <tr>
+                            <td>first name</td>
+                            <td>last name</td>
+                            <td>course</td>
+                            <td>country</td>
+                            <td>profile</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    {
+                            props.students.map((student, key) => {
+                                return (
+                                    <tr key={key}>
+                                        <td>{student.firstName}</td>
+                                        <td>{student.lastName}</td>
+                                        <td>{student.course}</td>
+                                        <td>{student.address.country}</td>
+                                        <td>
+                                            <button
+                                                id={student.studentId}
+                                                onClick={() => setStu(student)}>
+                                                View
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <div className='rightDiv'>
+               <Profile stu={stu}/>
+                
+                
+            </div>
+        </div>       
+    );
+}
